aab496275adba1a385c7d2d21cd517365aa796e9
danielMarques2006 <dfsm14.boy@gmail.com>
2021-02-18 16:40:17 +0100

    commit do mi parte 2

--- circunferencia.py
+++ circunferencia.py
@@ -1,5 +1,4 @@
-
 from math import pi
 
-raio = input("Qual é o raio da circunferência? ")
+raio = float(input("Introduza o raio da circunferência: "))
 print("A área da circunferência é de ", pi * float(raio **2))
