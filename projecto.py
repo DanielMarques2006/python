253452b038e8e81064055b05627e37471d3dac92
danielMarques2006 <dfsm14.boy@gmail.com>
2021-02-18 15:18:05 +0100

    adicionei algumas coisinhas

--- projecto.py
+++ projecto.py
@@ -1,10 +1,12 @@
-# Estes é uma nova alteração
+# Este é uma nova alteração
 
 salario = float(input("Digite o seu salário: "))
 despesas = float(input("Digite o valor das suas despesas: "))
 lucro = salario - despesas
 
 if lucro >= 0:
-    print("O deu lucro é de: ", lucro)
+    print("O seu lucro é de: ", lucro)
 else:
     print("O seu prejuizo é de: ", abs(lucro))  
+
+print("O imapcto percentual das despesas sobre o slario é de ", despesas * 100 // salario, "%")
