26e8ee8a1877ad09b3c9725eb4c9a4a07b2efaf9
danielMarques2006 <dfsm14.boy@gmail.com>
2021-02-18 14:56:15 +0100

    Primeiro commit

---
+++ projecto.py
@@ -0,0 +1,8 @@
+salario = float(input("Digite o seu salário: "))
+despesas = float(input("Digite o valor das suas despesas: "))
+lucro = salario - despesas
+
+if lucro >= 0:
+    print("O deu lucro é de: ", lucro)
+else:
+    print("O seu prejuizo é de: ", abs(lucro)) 
